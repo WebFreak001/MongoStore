b5bb5f6d6de17996f0cfcdc025e44cd0cc68d019
WebFreak001 <janju007@web.de>
2016-05-01 02:51:53 +0200

    Formatted code

--- source/mongostore.d
+++ source/mongostore.d
@@ -36,45 +36,50 @@ public:
 		_collection = collection;
 	}
 
-	@property SessionStorageType storageType() const { return SessionStorageType.bson; }
+	@property SessionStorageType storageType() const
+	{
+		return SessionStorageType.bson;
+	}
 
 	Session create()
 	{
 		auto s = createSessionInstance();
-		_collection.update(["id": s.id], Bson(["session": Bson(null)]), UpdateFlags.Upsert);
+		_collection.update(["id" : s.id], Bson(["session" : Bson(null)]), UpdateFlags.Upsert);
 		return s;
 	}
 
 	Session open(string id)
 	{
-		return _collection.findOne(["id": id]).isNull ? Session.init : createSessionInstance(id);
+		return _collection.findOne(["id" : id]).isNull ? Session.init : createSessionInstance(id);
 	}
 
 	void set(string id, string key, Variant value)
 	{
-		_collection.update(["id": id], Bson(["$set": Bson(["session." ~ key.makeKey: value.get!Bson])]), UpdateFlags.Upsert);
+		_collection.update(["id" : id],
+			Bson(["$set" : Bson(["session." ~ key.makeKey : value.get!Bson])]), UpdateFlags.Upsert);
 	}
 
 	Variant get(string id, string key, lazy Variant defaultVal)
 	{
-		auto v = _collection.findOne(["id": id])["session"].tryIndex(key.makeKey);
+		auto v = _collection.findOne(["id" : id])["session"].tryIndex(key.makeKey);
 		return v.isNull ? defaultVal : Variant(v.get);
 	}
 
 	bool isKeySet(string id, string key)
 	{
-		return !_collection.findOne(Bson(["id": Bson(id), "session." ~ key.makeKey: Bson(["$exists": Bson(true)])])).isNull;
+		return !_collection.findOne(Bson(["id" : Bson(id),
+			"session." ~ key.makeKey : Bson(["$exists" : Bson(true)])])).isNull;
 	}
 
 	void destroy(string id)
 	{
-		_collection.remove(["id": id]);
+		_collection.remove(["id" : id]);
 	}
 
 	int iterateSession(string id, scope int delegate(string key) del)
 	{
-		auto v = _collection.findOne(["id": id]);
-		foreach(string key, _; v["session"])
+		auto v = _collection.findOne(["id" : id]);
+		foreach (string key, _; v["session"])
 			if (auto ret = del(key))
 				return ret;
 		return 0;
@@ -86,9 +91,9 @@ private:
 
 private string makeKey(string key)
 {
-	if(key.length == 0)
+	if (key.length == 0)
 		return "_";
-	if(key[0] == '$')
+	if (key[0] == '$')
 		return "_" ~ key;
 	return key;
 }
